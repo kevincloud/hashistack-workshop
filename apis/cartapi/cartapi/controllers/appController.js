682a50ef0789d1574fb838cfccf227990f671e1e
Kevin Cochran <kcochran@hashicorp.com>
2019-07-14 13:39:58 -0400

    update

--- apis/cartapi/cartapi/controllers/appController.js
+++ apis/cartapi/cartapi/controllers/appController.js
@@ -12,7 +12,7 @@ var datetime = new Date().getTime().toString();
 exports.list_cart_items = function(req, res) {
     var sessionid = req.query.sessionId;
 
-    console.log("sessionid: " + req);
+    console.log("sessionid: " + JSON.stringify(req));
 
     ddb.query({
         TableName: table,
