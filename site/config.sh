38555682f8cba087ff670adfd5913d8023931735
Kevin Cochran <kcochran@hashicorp.com>
2019-08-05 16:29:00 -0400

    update

--- site/config.sh
+++ site/config.sh
@@ -5,10 +5,3 @@ cat<<EOF >>/etc/apache2/apache2.conf
         Require all granted
 </Directory>
 EOF
-
-cat<<EOF >>/etc/dnsmasq.conf
-server=/consul/127.0.0.1#8600
-server=169.254.169.253#53
-no-resolv
-log-queries
-EOF
